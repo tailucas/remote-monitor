ef2b082e1ac6e9399b174c44c0ac26356165d7c4
db2inst1 <db2inst1@webafrica.org.za>
2016-01-10 21:16:24 +0200

    post metered metric

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 set -o pipefail
-export PYTHONPATH="/app/libs/IOPi/:/app/libs/ADCPi/:${PYTHONPATH:-}"
+export PYTHONPATH="/app/libs/IOPi/:/app/libs/ADCPi/:/app/:${PYTHONPATH:-}"
 python "$@" 2>&1 | logger
