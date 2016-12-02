66e9f3ff79ae506614e6b6cd3c992679e53a888f
Tai Lucas <tglucas@gmail.com>
2016-12-02 20:09:05 +0000

    Further tweak timings.

--- sampler/sampler.ino
+++ sampler/sampler.ino
@@ -4,13 +4,13 @@ const byte interruptPin = 2;
 const byte dataPin = 3;
 // timing constants
 const int clock_duration = 220;
-const int clock_duration_tolerance = clock_duration/10;
 const int clock_preamble = clock_duration*2;
 const int clock_preamble_tolerance = clock_preamble/10;
 const int clock_idle = 6000;
+const int sample_count = 7;
+const int sample_threshold = sample_count / 2;
 // for printing
 unsigned long last_print_ts = 0;
-unsigned long last_print_interval = 0;
 const long print_interval = 10000000;
 // interrupt routine variables
 volatile unsigned long now = 0;
@@ -24,28 +24,38 @@ volatile unsigned long prev_data_word2 = 0;
 volatile boolean word_ready = false;
 volatile boolean in_word = false;
 volatile boolean changed = false;
+volatile int sample_iterator = 0;
+volatile int sample_value = 0;
 void int_clock() {
   // update timings
   now = micros();
   clock_interval = now - last_clock_ts;
-  // update timings
-  last_clock_ts = now;
   // check for the first clock with some tolerance
   if (clock_interval > (clock_preamble-clock_preamble_tolerance) && clock_interval < (clock_preamble+clock_preamble_tolerance)) {
     in_word = true;
     // reset the bit position unconditionally
     bit_pos = 0;
-  } else if (!in_word) {
-    return;
-  } else if (clock_interval > (clock_duration+clock_duration_tolerance) || clock_interval < (clock_duration-clock_duration_tolerance)) {
-    // abandon this word because the timing is off
-    in_word = false;
+  } else if (clock_interval < (clock_duration/2)) {
+    // ignore this clock because it is too short
     return;
   }
-  // allow the edge to dissipate
-  delayMicroseconds(100);
+  // update timings
+  last_clock_ts = now;
+  if (!in_word) {
+    return;
+  }
+  // sample the value
+  sample_value = 0;
+  for (sample_iterator=0; sample_iterator<sample_count; sample_iterator++) {
+    sample_value += digitalRead(dataPin);
+  }
+  if (sample_value < sample_threshold) {
+    sample_value = LOW;
+  } else {
+    sample_value = HIGH;
+  }
   // read the data (goes low) and shift
-  if (digitalRead(dataPin) == LOW) {
+  if (sample_value == LOW) {
     // set
     if (bit_pos < 32) {
       data_word1 |= ((unsigned long)1 << (31-bit_pos));
@@ -83,7 +93,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT);
-  pinMode(dataPin, INPUT_PULLUP);
+  pinMode(dataPin, INPUT);
   // older compiler
   //attachInterrupt(digitalPinToInterrupt(interruptPin), int_clock, CHANGE);
   //Board	        int.0	int.1	int.2	int.3	int.4	int.5
@@ -95,8 +105,7 @@ void loop() {
   // we're outside of the data word
   if (word_ready) {
     // print immediately upon change or regularly for heartbeat
-    last_print_interval = micros() - last_print_ts;
-    if (changed || (last_print_interval > print_interval)) {
+    if (changed || ((micros() - last_print_ts) > print_interval)) {
       Serial.print(data_word1, HEX);
       Serial.print(',');
       Serial.println(data_word2, HEX);
